009c03d18563b2260ce074c9d47825cbf7f157fe
romoguill <romoguill@gmail.com>
2025-05-29 21:16:34 -0300

    Created the entity and dto for basic crud

---
+++ src/schemas/tasks.ts
@@ -0,0 +1,25 @@
+import z from 'zod';
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const createTaskSchema = z.object({
+  title: z.string().trim().min(1).max(50),
+  description: z.string().trim().max(255).optional(),
+  completed: z.boolean().optional().default(false),
+});
+
+export const updateTaskSchema = z.object({
+  title: z.string().trim().min(1).max(50).optional(),
+  description: z.string().trim().max(255).optional(),
+  completed: z.boolean().optional(),
+});
+
+export type CreateTask = z.infer<typeof createTaskSchema>;
+export type UpdateTask = z.infer<typeof updateTaskSchema>;
