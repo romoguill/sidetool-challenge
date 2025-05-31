d2a9671c4c9b4ec72c22e6d04e440fc0e1096e2f
romoguill <romoguill@gmail.com>
2025-05-31 12:58:38 -0300

    Setup playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Home Page", () => {
+  test("should have metadata", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await expect(page).toHaveTitle("Sidetool - Tasks Management App");
+  });
+
+  test("should display tasks title", async ({ page }) => {
+    await page.goto("http://localhost:3000/home");
+
+    const taskTitle = await page.getByRole("heading", { name: "Tareas" });
+    await expect(taskTitle).toBeVisible();
+  });
+});
+
+// await page.goto('http://localhost:3000/home');
+//   await page.getByRole('link').click();
+//   await page.getByRole('textbox', { name: 'Título' }).fill('Nueva tarea');
+//   await page.getByRole('textbox', { name: 'Descripción' }).click();
+//   await page.getByRole('textbox', { name: 'Descripción' }).fill('Algo para hacer');
+//   await page.getByRole('button', { name: 'Crear' }).click();
+//   await page.getByRole('link', { name: 'Atrás' }).click();
+//   await page.locator('div').filter({ hasText: /^Nueva tarea$/ }).getByRole('button').click();
+//   await page.getByRole('menuitem', { name: 'Editar' }).click();
+//   await page.getByRole('textbox', { name: 'Título' }).fill('Nueva tarea modificada');
+//   await page.getByRole('button', { name: 'Actualizar' }).click();
+//   await page.locator('div:nth-child(2) > div > .peer').click();
+//   await page.locator('div:nth-child(4) > div > .peer').click();
+//   await page.locator('[id="radix-«r30»"]').click();
+//   await page.getByRole('menuitem', { name: 'Eliminar' }).click();
+//   await page.locator('div').filter({ hasText: 'Tareas4/' }).getByRole('button').click();
+//   await page.getByRole('menuitem', { name: 'Completadas' }).click();
+//   await page.locator('div').filter({ hasText: 'Tareas4/' }).getByRole('button').click();
+//   await page.getByRole('menuitem', { name: 'Incompletas' }).click();
+//   await page.locator('div').filter({ hasText: 'Tareas0/' }).getByRole('button').click();
